73948819b39337553bc3863c0144ffa8b2c3a711
jasonwangnanjing <jinqu7777@163.com>
2015-10-24 19:24:11 +0800

    vergin java

--- hello/src/main/java/com/bsh/cic/hello/App.java
+++ hello/src/main/java/com/bsh/cic/hello/App.java
@@ -1,13 +1,47 @@
 package com.bsh.cic.hello;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+ 
 /**
- * Hello world!
- *
+ * 2015-10-24 
+ * First Java try
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        try {            
+            BufferedReader br = new BufferedReader(new FileReader("D:\\test.txt"));
+            String s;
+            StringBuffer sb = new StringBuffer();
+        	Map<String,Integer> map = new HashMap<String, Integer>();
+        	
+            while ((s = br.readLine()) != null) {
+            
+                StringTokenizer st = new StringTokenizer(s.toString(),",.! \n");
+                while (st.hasMoreTokens()) {
+                    String letter = st.nextToken();
+                    int count;
+                    if (map.get(letter) == null) {
+                        count = 1;
+                    } else {
+                        count = map.get(letter).intValue() + 1;
+                    }
+                    map.put(letter,count);
+                }
+            	            
+        }
+                       
+            for (String key : map.keySet()) {
+               System.out.println( "Word" + "  " +  key + "  " + "Frequecy" + "  " + map.get(key));
+            }
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("文件未找到~！");
+        } catch (IOException e) {
+            System.out.println("文件读异常~！");
+        }
     }
 }
